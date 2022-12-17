4a2ce2609bc4508779c9070136b54bfdbbda0b03
mahfuz225bd <60539168+mahfuz225bd@users.noreply.github.com>
2022-12-17 22:16:39 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,140 +9,140 @@ import ShareLinks from './components/ShareLinks';
 import ButtonGroup from './components/ButtonGroup';
 
 export default class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			quotes: [],
-			quote: '',
-			author: '',
-			color: '',
-			disableButtons: false,
-			animatedQuote: false,
-			speaking: false,
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      quote: '',
+      author: '',
+      color: '',
+      disableButtons: false,
+      animatedQuote: false,
+      speaking: false,
+    };
 
-		this.refMsgCopied = createRef();
-	}
+    this.refMsgCopied = createRef();
+  }
 
-	componentDidMount() {
-		fetch(
-			'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-		)
-			.then((res) => res.json())
-			.then((data) => {
-				this.setState({
-					quotes: data.quotes,
-				});
+  componentDidMount() {
+    fetch(
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          quotes: data.quotes,
+        });
 
-				this.changeRandomQuote();
-			})
-			.catch((err) => console.error(err));
-	}
+        this.changeRandomQuote();
+      })
+      .catch((err) => console.error(err));
+  }
 
-	componentDidUpdate() {
-		// Changing random color
-		const root = document.querySelector(':root');
-		root.style.setProperty('--color', this.state.color);
-	}
+  componentDidUpdate() {
+    // Changing random color
+    const root = document.querySelector(':root');
+    root.style.setProperty('--color', this.state.color);
+  }
 
-	changeRandomQuote = () => {
-		// Disable buttons + enable animateQuote
-		this.setState({
-			disableButtons: true,
-			animatedQuote: true,
-		});
+  changeRandomQuote = () => {
+    // Disable buttons + enable animateQuote
+    this.setState({
+      disableButtons: true,
+      animatedQuote: true,
+    });
 
-		// Empty quote and author from state
-		this.setState({
-			quote: '',
-			author: '',
-		});
+    // Empty quote and author from state
+    this.setState({
+      quote: '',
+      author: '',
+    });
 
-		// Get random quote form state
-		const getRandomQuote =
-			this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
+    // Get random quote form state
+    const getRandomQuote =
+      this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
 
-		// Set random quote to state with delaying 1s and make disable=false of #btnNewQuote + remove .animated of #quoteSign
-		setTimeout(() => {
-			this.setState({
-				quote: getRandomQuote.quote,
-				author: getRandomQuote.author,
-				color: getRandomColor(),
-			});
+    // Set random quote to state with delaying 1s and make disable=false of #btnNewQuote + remove .animated of #quoteSign
+    setTimeout(() => {
+      this.setState({
+        quote: getRandomQuote.quote,
+        author: getRandomQuote.author,
+        color: getRandomColor(),
+      });
 
-			this.setState({
-				disableButtons: false,
-				animatedQuote: false,
-			});
-		}, 1000);
-	};
+      this.setState({
+        disableButtons: false,
+        animatedQuote: false,
+      });
+    }, 1000);
+  };
 
-	render() {
-		const { quote, author, disableButtons, animatedQuote, speaking } =
-			this.state;
-		const quoteNarration = `${author} said, "${quote}"`;
-		return (
-			<div className={styles['wrapper']}>
-				{/* Alert message, if copied */}
-				<div className={styles['msg-copied']} ref={this.refMsgCopied}>
-					<i className="fas fa-check-circle" aria-hidden="true"></i> Copied!
-				</div>
-				<main className={styles['quote-container']}>
-					{/* Quote body */}
-					<section className={styles['quote-body']}>
-						<p>
-							<span
-								className={
-									animatedQuote
-										? `${styles['quote-sign']} ${styles['animated']}`
-										: styles['quote-sign']
-								}
-							>
-								<i className="fas fa-quote-left" aria-hidden="true"></i>
-							</span>
-							{quote}
-						</p>
-						<span className={styles['author']}>- {author}</span>
-					</section>
+  render() {
+    const { quote, author, disableButtons, animatedQuote, speaking } =
+      this.state;
+    const quoteNarration = `${author} said, "${quote}"`;
+    return (
+      <div className={styles['wrapper']}>
+        {/* Alert message, if copied */}
+        <div className={styles['msg-copied']} ref={this.refMsgCopied}>
+          <i className="fas fa-check-circle" aria-hidden="true"></i> Copied!
+        </div>
+        <main className={styles['quote-container']}>
+          {/* Quote body */}
+          <section className={styles['quote-body']}>
+            <p>
+              <span
+                className={
+                  animatedQuote
+                    ? `${styles['quote-sign']} ${styles['animated']}`
+                    : styles['quote-sign']
+                }
+              >
+                <i className="fas fa-quote-left" aria-hidden="true"></i>
+              </span>
+              {quote}
+            </p>
+            <span className={styles['author']}>- {author}</span>
+          </section>
 
-					{/* Quote footer */}
-					<section className={styles['quote-footer']}>
-						<ShareLinks shareText={quoteNarration} hideLinks={disableButtons} />
-						<ButtonGroup
-							btnSpeakAction={() => {
-								this.setState({
-									speaking: true,
-								});
+          {/* Quote footer */}
+          <section className={styles['quote-footer']}>
+            <ShareLinks shareText={quoteNarration} hideLinks={disableButtons} />
+            <ButtonGroup
+              btnSpeakAction={() => {
+                this.setState({
+                  speaking: true,
+                });
 
-								const speak = textToSpeech(quoteNarration);
-								speak.onend = () => {
-									this.setState({
-										speaking: false,
-									});
-								};
-							}}
-							btnCopyAction={() => {
-								copyToClipboard(quoteNarration);
-								alert('Copied');
-							}}
-							btnNewQuoteAction={() => this.changeRandomQuote()}
-							disableButtons={disableButtons}
-							speaking={speaking}
-						/>
-					</section>
-				</main>
-				<footer>
-					by{' '}
-					<a
-						href="http://codepen.io/mahfuz225bd"
-						target="_blank"
-						rel="noreferrer"
-					>
-						Muhammad Sultan Al Mahfuz
-					</a>
-					.
-				</footer>
-			</div>
-		);
-	}
+                const speak = textToSpeech(quoteNarration);
+                speak.onend = () => {
+                  this.setState({
+                    speaking: false,
+                  });
+                };
+              }}
+              btnCopyAction={() => {
+                copyToClipboard(quoteNarration);
+                alert('Copied');
+              }}
+              btnNewQuoteAction={() => this.changeRandomQuote()}
+              disableButtons={disableButtons}
+              speaking={speaking}
+            />
+          </section>
+        </main>
+        <footer>
+          by{' '}
+          <a
+            href="http://codepen.io/mahfuz225bd"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Muhammad Sultan Al Mahfuz
+          </a>
+          .
+        </footer>
+      </div>
+    );
+  }
 }
